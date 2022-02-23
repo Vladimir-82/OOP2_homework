971e9cf36c9c6563a4edff6c0dbe86cae6e4cde7
vladimir <grun_gespenst@tut.by>
2022-02-23 22:30:11 +0300

    folder_file

--- Folder_file.py
+++ Folder_file.py
@@ -11,20 +11,20 @@ class Folder():
     def __init__(self, name, size):
         self.name = name
         self.size = size
+        self.content = []
 
 
 folder = Folder('My_folder', 20)
 
-Folder_content = []
-Folder_list = []
+dinamic_size = 0
 
 num = 1
-while sum(Folder_list) <= folder.size - 3:
+while dinamic_size <= folder.size - 3:
     file = File(name=f'File_name-{num}', size=random.randint(1, 3))
-    Folder_list.append(file.size)
-    Folder_content.append(file)
+    dinamic_size += file.size
+    folder.content.append(file)
     num += 1
 
-print(f'В папке {folder.name} {len(Folder_content)} файлов суммарным размером {sum(Folder_list)} Мб')
-for el in Folder_content:
+print(f'В папке {folder.name} {len(folder.content)} файлов суммарным размером {dinamic_size} Мб')
+for el in folder.content:
     print(f'Файл - {el.name}, размер файла - {el.size} Мб')
